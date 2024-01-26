fe250ae6e39c49337f57ce1d0a62717a0fad92f4
Caio Gabriel Barreto Balieiro <144500235+caiogbb@users.noreply.github.com>
2024-01-26 01:10:36 -0400

    Update ETL_data.R

--- ETL_data.R
+++ ETL_data.R
@@ -1,36 +1,41 @@
 library(sparklyr)
 
-# Inicializar uma sessão do Spark
+# Start the session in Spark
 sc <- spark_connect(master = "local", version = "3.5")
 
-# Substitua "caminho/do/arquivo/multimedia.csv" pelo caminho real do seu arquivo CSV
-caminho_do_arquivo <- "occurence.csv"
+# Int This case I cansider use the dataset in my local machine "occurence.csv"
+# caminho_do_arquivo <- "occurence.csv"
 
-# Ler o arquivo CSV para um DataFrame Spark
+# read a dataframe using spark 
 df_spark <- spark_read_csv(sc, "occurence_spark", caminho_do_arquivo)
 
-# Mostrar as primeiras linhas do DataFrame Spark
+df_spark <- df_spark %>%
+  filter(country == "Poland")
+
+# show the head of dataset
 head(df_spark)
 
-# Substitua "caminho/do/arquivo/salvo.csv" pelo caminho onde você deseja salvar o arquivo CSV
+# save the CSV consider only Poland
 caminho_do_arquivo_salvo <- "polandy/"
 
-# Salvar o DataFrame Spark em um arquivo CSV
+# Save the dataframe
 spark_write_csv(df_spark, path = caminho_do_arquivo_salvo, mode = "overwrite")
 
+# disconect session
 spark_disconnect(sc)
+
+# Create only a single CSV file
 
 arquivos_csv <- list.files('polandy/', pattern = "\\.csv$", full.names = TRUE)
 
 df_merged <- read.csv(arquivos_csv[1], h=T, sep = ',')
 
-# Loop para carregar e fazer join com os próximos arquivos
+# loop to build a single CSV file
 for (i in 2:length(arquivos_csv)) {
-  # Carregar o próximo arquivo CSV
   df_temp <- read.csv(arquivos_csv[i], h=T, sep = ',')
-  
   df_merged <- rbind(df_merged, df_temp)
 }
 
+#save the csv data that I use for the build of the bio App
 write.csv(df_merged, "arquivo.csv", row.names = FALSE)
 
